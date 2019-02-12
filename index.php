dc44024f3a617a91f89b90047efc1726607dd5ee
Toby Smith <tobysmith568@hotmail.co.uk>
2019-02-12 00:21:43 +0000

    Moved fixed response data

--- index.php
+++ index.php
@@ -2,19 +2,28 @@
 
 header('Content-Type: application/json');
 
+$result = get_default_result();
+
 $xml = simplexml_load_file('https://www.reddit.com/r/TellAlexa.rss');
 
-$dt = new DateTime();
-$dt->setTimeZone(new DateTimeZone('UTC'));
-
-$result = new stdClass();
-$result->uid = 'urn:uuid:' . uniqid();
-$result->updateDate = $dt->format('Y-m-d\TH:i:s.\0\Z');
+$result->titleText = 'The top post from r/TellAlexa';
 $result->mainText = (string) $xml->entry[0]->title;
 $result->redirectionUrl = 'https://reddit.com/r/TellAlexa/hot';
-
-$result = get_default_result();
 
 echo json_encode($result);
 
 die();
+
+
+
+
+function get_default_result() {
+	$dt = new DateTime();
+	$dt->setTimeZone(new DateTimeZone('UTC'));
+
+	$result = new stdClass();
+	$result->uid = 'urn:uuid:' . uniqid();
+	$result->updateDate = $dt->format('Y-m-d\TH:i:s.\0\Z');
+	
+	return $result;
+}
